34792fa68141d9c6d23e9c8d22c84d9735fa62b9
Vinny <1998vss@gmail.com>
2022-04-03 19:27:35 -0300

    iniciando navbar

--- src/components/Navbar/navbar index.js
+++ src/components/Navbar/navbar index.js
@@ -2,8 +2,9 @@ import "./navbar index.css"
 
 export default function Navbar(){
     return(
-        <div id="Navbardiv">
-            
+        <div id="navbarDiv">
+            <button id="homeButton"><span id="homeButtonText">MultiThings</span></button>
+                <input id="navSearch"type="search"/>   
         </div>
     )
 }
