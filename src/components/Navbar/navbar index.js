e4cd30226f4ca53c6add9c8804e7f82697564b55
Vinny <1998vss@gmail.com>
2022-04-04 22:21:56 -0300

    Embelezando Navbar

--- src/components/Navbar/navbar index.js
+++ src/components/Navbar/navbar index.js
@@ -3,8 +3,15 @@ import "./navbar index.css"
 export default function Navbar(){
     return(
         <div id="navbarDiv">
-            <button id="homeButton"><span id="homeButtonText">MultiThings</span></button>
-                <input id="navSearch"type="search"/>   
+            <div id="navbarOne">
+                <div id="homeButtonDiv"><a id="homeButton"><span id="homeButtonText">MultiThings</span></a></div>
+                <div id="navSearchDiv" ><input type="search" placeholder="Search for Multithings"/></div>
+            </div>
+            <div id="navbarTwo">
+                <div id="walletNinja-menu">3</div>
+                <div id="swissArmyKnife-menu">4</div>
+                <div id="furniture-menu">5</div> 
+            </div> 
         </div>
     )
 }
