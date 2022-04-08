614638cb140a974a3e6d18c1b8d1073150fc6617
Vinny <1998vss@gmail.com>
2022-04-07 22:01:58 -0300

    Tentativa de inserção de responsividade

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import "./App.css"
 import Navbar from "./components/Navbar/navbar index";
 
 function App() {
   return (
     <div className="App">
+      <div class="section group">
+        <div class="col one"></div>
+        <div class="col two"></div>
+        <div class="col three"></div>
+        <div class="col four"></div>
+        <div class="col five"></div>
+        <div class="col six"></div>
+        <div class="col seven"></div>
+        <div class="col eight"></div>
+        <div class="col nine"></div>
+        <div class="col ten"></div>
+        <div class="col eleven"></div>
+        <div class="col twelve"></div>
+      </div>
       <Navbar/>
     </div>
   );
